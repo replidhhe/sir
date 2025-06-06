dc95c60ef27ca59de4551e16141874e5ce703b06
replidhhe <rahaderb@gmail.com>
2025-06-06 23:50:02 +0600

    Update fuck.js

--- modules/commands/fuck.js
+++ modules/commands/fuck.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'fuckv2.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/Ee93Xjb.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.ibb.co/gbXCQr6D/8164e6aee2df.jpg", path);
 }
 
 async function makeImage({ one, two }) {

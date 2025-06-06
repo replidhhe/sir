db2e0a06ff2eebe0f8ba45aca2c8c2f5200d7200
replidhhe <rahaderb@gmail.com>
2025-06-06 23:47:52 +0600

    Update fingeringv2.js

--- modules/commands/fingeringv2.js
+++ modules/commands/fingeringv2.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'fingeringv2.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/CQQZusa.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/enqQKso.jpeg", path);
 }
 
 async function makeImage({ one, two }) {
